4200bf36743a9af93aa971a1801bd72fe387aba8
sudoaaditya <kashidadi@gmail.com>
2025-01-13 20:34:15 +0530

    planet partial

---
+++ src/components/classes/Planets.js
@@ -0,0 +1,15 @@
+
+
+class Planet {
+
+    constructor(props) {
+
+        this.name = props.name;
+        this.radius = props.radius;
+    }
+
+    createShape = () => {
+        //create sphere with radius
+        
+    }
+}
